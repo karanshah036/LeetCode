b10b183ee095e7f827bae8bf76f1177488f8b838
Karan Shah <karanshah036@gmail.com>
2022-12-30 16:58:47 -0500

    Duplicate Zeros Solution

---
+++ 1089. Duplicate Zeros/Duplicate-Zeros.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int zeros=0,pointer=arr.length-1;
+        for(int i=0;i<arr.length-zeros;i++){
+            if(arr[i]==0){
+                zeros++;
+                if(i==arr.length-zeros){
+                    arr[pointer--]=0;
+                    zeros--;
+                    break;
+                }
+            }
+        }
+        int end=pointer-zeros;
+        for(int i=end;i>=0;i--){
+            if(arr[i]==0){
+                arr[pointer--]=0;
+                arr[pointer--]=0;
+            }else{
+                arr[pointer--]=arr[i];
+            }
+        }
+    }
+}
