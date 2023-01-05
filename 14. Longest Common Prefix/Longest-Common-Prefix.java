348877f51d1160a5213668095cde40be58bbfb4c
Karan Shah <karanshah036@gmail.com>
2023-01-04 22:57:01 -0500

    Longest Common Prefix Solution

---
+++ 14. Longest Common Prefix/Longest-Common-Prefix.java
@@ -0,0 +1,16 @@
+// Time Complexity O(S) S-sum of all Strings
+// Space Complexity O(1)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0) return "";
+        for(int i=0;i<strs[0].length();i++){
+            char c = strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i==strs[j].length() || strs[j].charAt(i)!=c){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
