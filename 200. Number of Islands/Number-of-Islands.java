bf0bae99ff106ef643ed12f0c42b5102e2a306b1
Karan Shah <karanshah036@gmail.com>
2023-01-19 10:39:12 -0500

    Update Number-of-Islands.java

--- 200. Number of Islands/Number-of-Islands.java
+++ 200. Number of Islands/Number-of-Islands.java
@@ -1,3 +1,4 @@
+// DFS Approach
 // Time Complexity O(M x N)
 // Space Complexity O(M x N)
 class Solution {
@@ -22,5 +23,50 @@ class Solution {
             dfs(grid,row,col+1);
             dfs(grid,row,col-1);
         }
+    }
+}
+
+// BFS Approach
+// Time Complexity O(M x N)
+// Space Complexity O(min(M,N))
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid.length==0 || grid==null) return 0;
+        int nr=grid.length;
+        int nc=grid[0].length;
+        int numIslands=0;
+        for(int i=0;i<nr;i++){
+            for(int j=0;j<nc;j++){
+                if(grid[i][j]=='1'){
+                    numIslands++;
+                    grid[i][j]='0';
+                    Queue<Integer> queue = new LinkedList<>();
+                    int position = i*nc+j;
+                    queue.add(position);
+                    while(!queue.isEmpty()){
+                        int startinPoint = queue.poll();
+                        int row = startinPoint/nc;
+                        int col = startinPoint%nc;
+                        if(row+1<nr && grid[row+1][col]=='1'){
+                            queue.add((row+1) * nc + col);
+                            grid[row+1][col]='0';
+                        }
+                        if(row-1>=0 && grid[row-1][col]=='1'){
+                            queue.add((row-1) * nc + col);
+                            grid[row-1][col]='0';
+                        }
+                        if(col+1<nc && grid[row][col+1]=='1'){
+                            queue.add(row * nc + col+1);
+                            grid[row][col+1]='0';
+                        }
+                        if(col-1>=0 && grid[row][col-1]=='1'){
+                            queue.add((row) * nc + col-1);
+                            grid[row][col-1]='0';
+                        }
+                    }
+                }
+            }
+        }
+        return numIslands;
     }
 }
